e9a4466f4c0ebd06a7db52b92093e684eda89afc
cgil <lao.tseu.is.alive@gmail.com>
2023-01-27 17:01:08 +0100

    initial commit

---
+++ pkg/metadata/metadata.go
@@ -0,0 +1,27 @@
+package metadata
+
+const (
+	defaultServiceSchema = "public"
+	CountMetaSQL         = "SELECT COUNT(*) as num FROM go_metadata_db_schema;"
+	countMetaServiceSQL  = "SELECT COUNT(*) as num FROM go_metadata_db_schema WHERE service = $1;"
+	selectMetaSQL        = "SELECT  id, service, schema, table_name, version FROM go_metadata_db_schema WHERE service = $1"
+	insertMetaSQL        = "INSERT INTO go_metadata_db_schema (service, schema, table_name, version) VALUES ($1,$2,$3,$4)"
+	CreateMetaTable      = `
+CREATE TABLE IF NOT EXISTS go_metadata_db_schema
+(
+    id          serial    CONSTRAINT go_metadata_db_schema_pk   primary key,
+    service     text                             not null,
+    schema      text      default 'public'::text not null,
+    table_name  text                             not null,
+    version     text                             not null,
+    create_time timestamp default now()          not null,
+    CONSTRAINT go_metadata_db_schema_unique_service_schema_table
+        unique (service, schema, table_name)
+);
+comment on table go_metadata_db_schema is 'to track version of schema of different micro services';
+`
+	countMetaUserServiceSQL = "SELECT COUNT(*) as num FROM go_metadata_db_schema WHERE service = 'GO_USER';"
+	insertMetaUserService   = `INSERT INTO go_metadata_db_schema 
+						(service, schema, table_name, version) 
+				VALUES 	('GO_USER','public','go_user',$1) RETURNING id;`
+)
---
+++ pkg/version/version.go
@@ -0,0 +1,10 @@
+package version
+
+var (
+	APP        = "goCloudK8sCommonLibs"
+	AppSnake   = "go-cloud-k8s-common-libs"
+	VERSION    = "0.0.1"
+	REPOSITORY = "github.com/lao-tseu-is-alive/go-cloud-k8s-common-libs"
+	REVISION   = "unknown"
+	BuildStamp = "unknown"
+)
