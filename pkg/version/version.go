bd04fd594d56c45b6bfc4c45555f9cf3d28c48ec
cgil <lao.tseu.is.alive@gmail.com>
2023-02-21 17:11:46 +0100

    first test

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "goCloudK8sCommonLibs"
 	AppSnake   = "go-cloud-k8s-common-libs"
-	VERSION    = "0.0.3"
+	VERSION    = "0.0.4"
 	REPOSITORY = "github.com/lao-tseu-is-alive/go-cloud-k8s-common-libs"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
