1a3467a611b00d1bc941c04d11be6075fb06aa77
cgil <lao.tseu.is.alive@gmail.com>
2025-07-18 08:37:48 +0200

    updated version info

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -5,6 +5,6 @@ var (
 	AppSnake   = "go-cloud-k8s-common-libs"
 	VERSION    = "0.3.21"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-cloud-k8s-common-libs"
-	REVISION   = "v0.3.20"             // Will be set to a valid git revision at compile time
-	BuildStamp = "2025-07-17T10:07:43" // Will be set to a valid timestamp at compile time
+	REVISION   = "v0.3.21"             // Will be set to a valid git revision at compile time
+	BuildStamp = "2025-07-18T08:37:48" // Will be set to a valid timestamp at compile time
 )
