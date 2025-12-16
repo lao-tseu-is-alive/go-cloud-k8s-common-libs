341a02bf3d411a3870d48ed664b307a46fede574
cgil <lao.tseu.is.alive@gmail.com>
2025-12-16 13:06:07 +0100

    chore: update build info for v0.6.2

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -5,6 +5,6 @@ var (
 	AppSnake   = "go-cloud-k8s-common-libs"
 	VERSION    = "0.6.2"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-cloud-k8s-common-libs"
-	REVISION   = "v0.6.1"
-	BuildStamp = "2025-12-16T10:17:57+0100"
+	REVISION   = "v0.6.2"
+	BuildStamp = "2025-12-16T13:06:07+0100"
 )
