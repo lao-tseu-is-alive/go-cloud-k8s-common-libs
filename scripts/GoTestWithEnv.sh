c58f7b505c43dfa05b55c17fd7eb9364fbd6f28b
cgil <lao.tseu.is.alive@gmail.com>
2025-12-16 14:53:28 +0100

    corrected bug

--- scripts/GoTestWithEnv.sh
+++ scripts/GoTestWithEnv.sh
@@ -7,19 +7,19 @@ LDFLAGS="-X ${APP_REPOSITORY}/pkg/version.BuildStamp=${NOW} -X ${APP_REPOSITORY}
 ENV_FILENAME='.env'
 
 if [[ $# -eq 1 ]]; then
-  ENV_FILENAME=${1:-.env}    # ← fixed the default value syntax
+  ENV_FILENAME=${1:-.env}    
 else
   echo "## 💥💥 expecting first argument to be path to an .env file name (default is .env in current directory)"
   exit 1
 fi
 
-echo "## will try to run : go test -race -coverprofile coverage.out -v ./... with env variables in ${ENV_FILENAME} ..."
+echo "## will try to run : go test -race -coverprofile=coverage.txt ./cmd/goCloudK8sExampleServer/... ./pkg/... with env variables in ${ENV_FILENAME} ..."
 
 if [[ -r "$ENV_FILENAME" ]]; then
-    echo "## will do : go test -race -coverprofile=coverage.txt -v ./... "
+    echo "## will do : go test -race -coverprofile=coverage.txt ./cmd/goCloudK8sExampleServer/... ./pkg/..."
     set -a
     source <(sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g" $ENV_FILENAME )
-    go test -race -coverprofile=coverage.txt -v ./...
+    go test -race -coverprofile=coverage.txt ./cmd/goCloudK8sExampleServer/... ./pkg/...
     set +a
 else
   echo "## 💥💥 env path argument : ${ENV_FILENAME} was not found"
